4409b0d407c47d88ee29ad088697e67260d7395c
Wilfried <la2spaille@gmail.com>
2022-03-12 17:39:36 +0100

    Update panier.svelte

--- src/components/panier.svelte
+++ src/components/panier.svelte
@@ -1,20 +1,7 @@
-<div>
-    <img src="./icons/cart.svg" alt="cart">
+<div class="w-">
+
 </div>
 
 <style lang="scss">
     @import "./../styles/settings";
-    div{
-        cursor: pointer;
-        position: fixed;
-        margin: 1rem;
-        top: 50%;
-        width: 50px;
-        height: 50px;
-        background: $orange;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        border-radius: 50%;
-    }
 </style>
